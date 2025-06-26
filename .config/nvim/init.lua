5ce82a47315a960d424a39b83acaec02ccccfca8
Marcel Zwart <marcelzwart92@gmail.com>
2025-06-26 20:55:58 +0200

    Yank highlighting for neovim.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,3 +38,14 @@ vim.cmd.colorscheme("tokyonight")
 
 -- set os clipboard
 vim.opt.clipboard = "unnamedplus"
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
