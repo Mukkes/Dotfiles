852e505c8d1af9712df916858b3b27da5cbf1d6d
Marcel Zwart <marcelzwart92@gmail.com>
2025-12-11 09:10:21 +0100

    require('lspconfig') is deprecated, use vim.lsp.config instead.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,7 +19,8 @@ return {
 
         config = function()
             local capabilities = require('blink.cmp').get_lsp_capabilities()
-            require("lspconfig").lua_ls.setup { capabilities = capabilities }
+            vim.lsp.config('lua_ls', { capabilities = capabilities })
+            --vim.lsp.enable('lua_ls')
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('my.lsp', {}),
