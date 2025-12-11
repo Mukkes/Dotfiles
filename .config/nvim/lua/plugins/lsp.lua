b8cc022a756d5dae37989fe361da0e9cdb85ca42
Marcel Zwart <marcelzwart92@gmail.com>
2025-12-11 09:13:28 +0100

    Enable lua_ls.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -20,7 +20,7 @@ return {
         config = function()
             local capabilities = require('blink.cmp').get_lsp_capabilities()
             vim.lsp.config('lua_ls', { capabilities = capabilities })
-            --vim.lsp.enable('lua_ls')
+            vim.lsp.enable('lua_ls')
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('my.lsp', {}),
