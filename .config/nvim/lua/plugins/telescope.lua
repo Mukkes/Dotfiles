3ee6424ce56a203e1de3dc755b1e3812bbddd801
Marcel Zwart <marcelzwart92@gmail.com>
2026-02-13 15:22:46 +0100

    Add telescope shortcuts for finding buffers, symbols and references.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -31,6 +31,12 @@ return {
             vim.keymap.set("n", "<leader>f", require('telescope.builtin').find_files)
             vim.keymap.set("n", "<leader>h", require('telescope.builtin').help_tags)
             vim.keymap.set("n", "<leader>g", require('telescope.builtin').live_grep)
+            vim.keymap.set("n", "<leader>b", require('telescope.builtin').buffers)
+            vim.keymap.set("n", "<leader>s", require('telescope.builtin').lsp_document_symbols)
+            -- Works only for open files/files in buffer.
+            -- Apparently this has something to do with clang.
+            -- It might just work with other lsp.
+            vim.keymap.set("n", "<leader>r", require('telescope.builtin').lsp_references)
         end
     }
 }
